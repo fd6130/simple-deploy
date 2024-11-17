ebe6c197cd080b8ec603aab0d59f6cda00292e5b
Vui <11419744+fd6130@users.noreply.github.com>
2024-11-17 23:11:48 +0800

    Update Laravel.php

--- app/Console/Commands/Recipes/Laravel.php
+++ app/Console/Commands/Recipes/Laravel.php
@@ -39,7 +39,7 @@ class Laravel implements Recipes
     {
         $this->command->info('[Finish Deploy] Executing now...');
 
-        $shell = !empty($this->config['finish_deploy']) ? $this->config['finish_deploy'] : "composer install --optimize-autoloader --no-dev; php artisan migrate --force; php artisan optimize:clear; php artisan optimize; sudo chown -R \$USER:www-data storage; sudo chown -R \$USER:www-data bootstrap/cache; sudo chmod -R 775 storage; sudo chmod -R 775 bootstrap/cache;";
+        $shell = !empty($this->config['finish_deploy']) ? $this->config['finish_deploy'] : "composer install --optimize-autoloader --no-dev; php artisan migrate --force; sudo chown -R \$USER:www-data storage; sudo chown -R \$USER:www-data bootstrap/cache; sudo chmod -R 775 storage/; sudo chmod -R 775 bootstrap/; php artisan cache:clear; php artisan optimize:clear; php artisan optimize;";
 
         Process::timeout(180)->path($this->config['path'])->run($shell, function (string $type, string $output)
         {
